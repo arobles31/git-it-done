9c4960235adef65220f5cca5c2c8b6885d555e07
Alejandro Robles <arobles2828@gmail.com>
2021-03-15 20:36:38 -0700

    fixed code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,10 +4,13 @@ var getUserRepos = function(user) {
 
   // make a request to the url
   fetch(apiUrl).then(function(response) {
+      console.log(response);
     response.json().then(function(data) {
       console.log(data);
     });
   });
 };
+
+getUserRepos("lernantino")
 
 
